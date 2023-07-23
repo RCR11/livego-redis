7d3a8de0417c0d45aafba34c2b7a91936a77c8ce
RCR11 <123052024+RCR11@users.noreply.github.com>
2023-07-23 12:09:29 -0400

    Randomize StreamKey each server boot
    https://github.com/Glamorgann/livego/blob/master/utils/uid/rand.go

--- utils/uid/rand.go
+++ utils/uid/rand.go
@@ -1,11 +1,17 @@
+//Randomizes StreamKeys each server boot - https://github.com/Glamorgann/livego
 package uid
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 var letterRunes = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) string {
-	b := make([]rune, n)
+	b := make([]ruhttps://github.com/Glamorgann/livegone, n)
+	// avoid generating same code in order !
+	rand.Seed(time.Now().UnixNano())
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
